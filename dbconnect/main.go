ab4509d623c140e0c74db6685ec65ed93c23e707
Akirapearl <draculaspot@hotmail.com>
2025-01-09 11:20:05 +0100

    Finished tutorial & polishing SQL statements

--- dbconnect/main.go
+++ dbconnect/main.go
@@ -45,6 +45,22 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := getAlbumByID(db, 3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
+
+	albID, err := addAlbum(db, Album{
+		Title:  "13 Voices",
+		Artist: "Sum 41",
+		Price:  10,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albID)
 }
 
 func getAlbumsByArtist(db *sql.DB, name string) ([]Album, error) {
@@ -72,4 +88,31 @@ func getAlbumsByArtist(db *sql.DB, name string) ([]Album, error) {
 		return nil, fmt.Errorf("getAlbumsByArtist %q: %v", name, err)
 	}
 	return albums, nil
+}
+
+func getAlbumByID(db *sql.DB, id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM Albums WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
+
+}
+
+func addAlbum(db *sql.DB, alb Album) (int64, error) {
+	//Missing max id search to automate insert
+	result, err := db.Exec("INSERT INTO Albums VALUES (7, ?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
 }
