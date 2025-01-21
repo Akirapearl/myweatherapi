58489fdbb59e029a141f3302eb00ee82788bc35b
Akirapearl <draculaspot@hotmail.com>
2025-01-21 12:20:23 +0100

    Unnecesary directory - Moved to /client

--- internal/internal.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-func GetAPIKey() string {
-	// This function is intended to hide or
-	//avoid directly sharing the API keys utilized for this
-	// or any other API client project.
-	apiKey := ""
-	return apiKey
-}
